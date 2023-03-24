667b70fc035cdd6611388ca484bb7e8e0ac395b5
LioraOceacovschi <liora.skliar@gmail.com>
2023-03-24 21:49:55 +0300

    CW_9_Wait
    First part of lesoon9

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -2,20 +2,35 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class RegistrationTests {
 
     WebDriver wd;
 
+
+
     @BeforeMethod
     public void init(){
         wd = new ChromeDriver();
         wd.navigate().to("https://telranedu.web.app/home");
+
+          //Implicit wait:
+        //  1 . wait for WebElement
+            //  wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //  2. wait for loading page
+            //  wd.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+
+
     }
 
 
@@ -42,48 +57,30 @@ public class RegistrationTests {
         // 3. submit by click registration button
         wd.findElement(By.xpath("//button[2]")).click();
 
+
+       // pause(5);
         // 4. assert
-        Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
+        //Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
 
+        // Explicit wait
+        WebElement elementAdd = (new WebDriverWait(wd,5)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='ADD']"))));
+        Assert.assertTrue(elementAdd.getText().equals("ADD"));
 
     }
 
-    //HW_8 Создать 2 негативных
-    // тестовых метода (неверный email, неверный password) на регистрацию в классе RegistrationTests
+    public void pause(int time){
+        // Wait  Option 1 
+//        try {
+//            Thread.sleep(time);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+        // Wait  Option 2
+//        FluentWait<WebDriver> wait = new FluentWait<>(wd);
+//        wait.withTimeout(Duration.ofSeconds(time));
 
-    @Test
-    public void registrationWrongEmail(){
-        int i = (int)(System.currentTimeMillis()/1000)%3600;
-        wd.findElement(By.xpath("//a[@href='/login']")).click();
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("ab" + i + "@m.r");
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("ab@m.r");
-
-        wd.findElement(By.xpath("//button[2]")).click();
     }
-    @Test
-    public void registrationWrongPass(){
-        int i = (int)(System.currentTimeMillis()/1000)%3600;
-        wd.findElement(By.xpath("//a[@href='/login']")).click();
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("abcde" + i + "@mail.ru");
-
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("1234");
-
-        wd.findElement(By.xpath("//button[2]")).click();
-    }
-
 
     @AfterMethod
     public void tearDown(){
