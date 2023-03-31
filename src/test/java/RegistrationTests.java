58ae99275962e9636e46ad42bb189722ad89dc3e
LioraOceacovschi <liora.skliar@gmail.com>
2023-03-31 22:24:37 +0300

    HW_9

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -7,8 +7,6 @@ import org.testng.annotations.Test;
 
 public class RegistrationTests extends TestBase{
 
-//    WebDriver wd;
-//    FluentWait<WebDriver> wait;
 
     @BeforeMethod
     public void preCondition(){
@@ -16,75 +14,38 @@ public class RegistrationTests extends TestBase{
             logout();
         }
     }
-//    public void init(){
-//        wd = new ChromeDriver();
-//        wd.navigate().to("https://telranedu.web.app/home");
-////        wait = new FluentWait<>(wd);
-//        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
-//    }
-
-
     @Test
     public void regPositiveTest(){
 
 //        1. open login/registration form
         openLoginRegistrationForm();
-//        WebElement loginBtn = wd.findElement(By.xpath("//a[@href='/login']"));
-//        loginBtn.click();
+
 //        2. fill login/registration form
         int i = (int)(System.currentTimeMillis() / 1000) % 3600;
         String email = "name" + i + "@mail.com";
         String password = "$Abcd1234";
         fillLoginRegistrationForm(email, password);
-//        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-//        emailInput.click();
-//        emailInput.clear();
-//        emailInput.sendKeys("abc" + i + "@def.com");
-//
-//        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-//        passInput.click();
-//        passInput.clear();
-//        passInput.sendKeys("$Abcd1234");
-//        3. submit by click registration button
-//        wd.findElement(By.xpath("//button[2]")).click();
-        submitRegistration();
 
-//        pause(5);
+//        3. submit by click registration button
+        submitRegistration();
 
 //        4. assert
         Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
     }
 
-//    public void pause(int time){
-//        try {
-//            Thread.sleep(time);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-
-//        FluentWait<WebDriver> wait = new FluentWait<>(wd);
-//        wait.withTimeout(Duration.ofSeconds(time));
-//    }
 
     @Test
     public void registrationWrongEmail(){
 //        1. open login/registration form
-        WebElement loginBtn = wd.findElement(By.xpath("//a[@href='/login']"));
-        loginBtn.click();
+        openLoginRegistrationForm();
 //        2. fill login/registration form
         int i = (int)(System.currentTimeMillis() / 1000) % 3600;
-        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys("abc" + i + "def.com");
+        String email = "name" + i + "@mail.c";
+        String password = "$Abcd1234";
+        fillLoginRegistrationForm(email,password);
 
-        WebElement passInput = wd.findElement(By.xpath("//input[2]"));
-        passInput.click();
-        passInput.clear();
-        passInput.sendKeys("$Abcd1234");
 //        3. submit by click registration button
-        wd.findElement(By.xpath("//button[2]")).click();
+        submitRegistration();
 //        4. assert
 //        Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
 
