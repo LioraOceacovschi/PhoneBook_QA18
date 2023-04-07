8fa13fae63793fe34eaa53ceb92e50350dfe0605
LioraOceacovschi <liora.skliar@gmail.com>
2023-04-07 22:59:33 +0300

    CW_10.1_Architecture_10.03.23

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,7 +1,7 @@
+package tests;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -10,50 +10,47 @@ public class RegistrationTests extends TestBase{
 
     @BeforeMethod
     public void preCondition(){
-        if(isLogged()){
-            logout();
+        if(app.getUser().isLogged()){
+            app.getUser().logout();
         }
     }
     @Test
     public void regPositiveTest(){
 
 //        1. open login/registration form
-        openLoginRegistrationForm();
+        app.getUser().openLoginRegistrationForm();
 
 //        2. fill login/registration form
         int i = (int)(System.currentTimeMillis() / 1000) % 3600;
         String email = "name" + i + "@mail.com";
         String password = "$Abcd1234";
-        fillLoginRegistrationForm(email, password);
+        app.getUser().fillLoginRegistrationForm(email, password);
 
 //        3. submit by click registration button
-        submitRegistration();
+        app.getUser().submitRegistration();
 
 //        4. assert
-        Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
+        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//a[text()='ADD']")));
+
     }
 
 
     @Test
     public void registrationWrongEmail(){
 //        1. open login/registration form
-        openLoginRegistrationForm();
+        app.getUser().openLoginRegistrationForm();
 //        2. fill login/registration form
         int i = (int)(System.currentTimeMillis() / 1000) % 3600;
         String email = "name" + i + "@mail.c";
         String password = "$Abcd1234";
-        fillLoginRegistrationForm(email,password);
+        app.getUser().fillLoginRegistrationForm(email,password);
 
 //        3. submit by click registration button
-        submitRegistration();
+        app.getUser().submitRegistration();
 //        4. assert
-//        Assert.assertTrue(wd.findElement(By.xpath("//a[text()='ADD']")).getText().equals("ADD"));
+        //Assert.assertFalse(app.getUser().isElementPresent(By.xpath("//a[text()='ADD']")));
 
 
     }
 
-    @AfterMethod
-    public void tearDown() {
-//        wd.quit();
-    }
 }
