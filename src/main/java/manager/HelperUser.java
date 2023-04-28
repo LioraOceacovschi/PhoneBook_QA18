74f72778e7367473968ea0e361ceab4f6cef39f5
LioraOceacovschi <liora.skliar@gmail.com>
2023-04-28 19:53:32 +0300

    CW_13_21.03.23

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,5 +1,6 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -27,6 +28,10 @@ public class HelperUser extends HelperBase {
         type(By.xpath("//input[1]"), email);
         type(By.xpath("//input[2]"), password);
     }
+    public void fillLoginRegistrationForm(User user) {
+        type(By.xpath("//input[1]"), user.getEmail());
+        type(By.xpath("//input[2]"), user.getPassword());
+    }
 
     public void submitRegistration() {
         click(By.xpath("//button[2]"));
@@ -35,4 +40,6 @@ public class HelperUser extends HelperBase {
     public void submitLogin() {
         click(By.xpath("//button[1]"));
     }
+
+
 }
