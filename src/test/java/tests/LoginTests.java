ae13941b17a4ecc10b4621345da0cb0f99e45462
LioraOceacovschi <liora.skliar@gmail.com>
2023-05-11 00:20:35 +0300

    CW_17_ListenerNG

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,16 +1,18 @@
 package tests;
 
+import manager.NGListener;
 import models.User;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
+@Listeners(NGListener.class)
 public class LoginTests extends TestBase {
 
     @Test
     public void loginPositiveTest() {
         User user = User.builder()
-                .email("@abcdef.com")
+                .email("abc@def.com")
                 .password("$Abcdef12345")
                 .build();
         app.getUser().openLoginRegistrationForm();
